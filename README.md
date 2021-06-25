97d14cc00694a4e0e86654d0e09b84d202c8d117
Luis Garcia <lgarcia29@eagles.ewu.edu>
2021-06-24 17:48:41 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+﻿In this assignment we are going to build out some of the logic needed for Secret Santa.
+There have been several updates to the SecretSanta repository.
+Key changes to be aware of:
+- The data project has been expanded and contains users for groups and a new Assignment class.
+- There is a new `GroupRepository` added to the business project.
+- The gifts section links has been removed from the web project. The pages are still present but should not be needed.
+- Opening the "edit" page of a group now also shows the users in a group, and has separate controls for editing the group and adding and removing users from the group.
+- No additional Web or E2E tests are needed for this assignment.
+
+# Assignment
+- Update the `GroupRepository` with functionality to generate gift assignments. ✔❌
+  - A group with with 2 or fewer users should result in an error. This error should be displayed to a user. ✔❌
+  - A user is not allowed to be both the Giver and Recipient of the assignment. ✔❌
+- Update the GroupsController with an endpoint for generating the assignments. ✔❌
+- Update the Groups/Edit page to contain the UI elements for the following
+  - A means to generate assignments. ✔❌
+  - A means to display any errors that occurs while generating assignments. ✔❌
+  - Display of the assignments for each of the users. ✔❌
+- Write appropriate unit tests for changes made to the `GroupRepository` ✔❌
+- Write appropriate functional tests for the change made to the `GroupsController` ✔❌
+
+## Extra Credit
+- There is currently no way to for a user to add gifts. On the edit user page add functionality to be able to add a gift to the user. ✔❌
+- Allow for navigating to a user from the Group edit page to make it easy to see the gifts requested by a user. ✔❌
---
+++ SecretSanta/src/SecretSanta.Business/GroupRepository.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata;
+using SecretSanta.Data;
+
+namespace SecretSanta.Business
+{
+    public class GroupRepository : IGroupRepository
+    {
+        private Random rng = new Random();
+        public Group Create(Group item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            MockData.Groups[item.Id] = item;
+            return item;
+        }
+
+        public Group? GetItem(int id)
+        {
+            if (MockData.Groups.TryGetValue(id, out Group? user))
+            {
+                return user;
+            }
+            return null;
+        }
+
+        public ICollection<Group> List()
+        {
+            return MockData.Groups.Values;
+        }
+
+        public bool Remove(int id)
+        {
+            return MockData.Groups.Remove(id);
+        }
+
+        public void Save(Group item)
+        {
+            if (item is null)
+            {
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            MockData.Groups[item.Id] = item;
+        }
+
+        public AssignmentResult GenerateAssignments(int id)
+        {
+            //grab and do some checks
+            Group group = GetItem(id);
+
+            if (group is null)
+            {
+                throw new NullReferenceException();
+            }
+            if (group.Users.Count < 3)
+            {
+                return AssignmentResult.Error("Sorry, there must be a minimum of three members in a group.");
+            }
+            //begin randomizing.
+            group.Assignments.Clear();
+
+            List<User> randomizedReceiverList = group.Users;
+            Shuffle(randomizedReceiverList);
+            
+            for (int i = 0; i < group.Users.Count; i++)
+            {
+                MockData.Groups[id].Assignments.Add(i < randomizedReceiverList.Count - 1
+                    ? new Assignment(randomizedReceiverList[i], randomizedReceiverList[i + 1])
+                    : new Assignment(randomizedReceiverList[i], randomizedReceiverList[0]));
+            }
+            return AssignmentResult.Success();
+        }
+
+        //list shuffling method from stack overflow
+        //source - https://stackoverflow.com/questions/273313/randomize-a-listt
+        private void Shuffle<T>(IList<T> list)
+        {
+            if (list == null)
+            {
+                throw new NullReferenceException();
+            }
+          
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = rng.Next(n + 1);
+                T value = list[k];
+                list[k] = list[n];
+                list[n] = value;
+            }
+        }
+    }
+}
