3260bf7ba628b64f1d41cd6a47b2b8905f8866cd
Luis Garcia <lgarcia29@eagles.ewu.edu>
2021-12-30 16:20:59 -0800

    Working to get webpage up on azure

--- SecretSanta/src/SecretSanta.Web/Startup.cs
+++ SecretSanta/src/SecretSanta.Web/Startup.cs
@@ -12,7 +12,8 @@ namespace SecretSanta.Web
     {
         private static HttpClient UsersHttpClient { get; } = new()
         {
-            BaseAddress = new Uri("https://localhost:5101/")
+            // BaseAddress = new Uri("https://localhost:5101/")
+            BaseAddress = new Uri("https://lgarcia29-secretsanta-api.azurewebsites.net/")
         };
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
@@ -25,10 +26,10 @@ namespace SecretSanta.Web
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            // if (env.IsDevelopment())
+            // {
                 app.UseDeveloperExceptionPage();
-            }
+            // }
 
             app.UseRouting();
 
